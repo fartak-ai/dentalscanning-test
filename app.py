32ae3cd52368ac78b51c60f04c1681fa767e1bc7
fartak-ai <141265164+fartak-ai@users.noreply.github.com>
2023-10-02 17:04:20 +0330

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
         config['preauthorized']
         )
 
-    st.write(authenticator)
+    # st.write(authenticator)
 
     #Home button will be in the middle of the nav list now
     app.add_app("Home", icon="🏠", app=apps.HomeApp(user_access_level, title='Home'), is_home=True)
